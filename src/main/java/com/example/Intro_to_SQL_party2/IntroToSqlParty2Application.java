b6f5018edc7ff549521c9c3b09b74a734af57dc1
DiplineRU <artgul52@gmail.com>
2025-02-21 16:50:31 +0300

    initial commit

---
+++ src/main/java/com/example/Intro_to_SQL_party2/IntroToSqlParty2Application.java
@@ -0,0 +1,15 @@
+package com.example.Intro_to_SQL_party2;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class IntroToSqlParty2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSqlParty2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Intro_to_SQL_party2/repository/FacultyRepository.java
@@ -0,0 +1,16 @@
+package com.example.Intro_to_SQL_party2.repository;
+
+import com.example.Intro_to_SQL_party2.model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+    Collection<Faculty> findByNameIgnoreCase(String name);
+
+    Collection<Faculty> findByColorIgnoreCase(String color);
+
+}
